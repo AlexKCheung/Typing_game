ccc9ce0720b8f31baa81f80973048373072ad1c3
Alex <alexcheung880@gmail.com>
2021-01-10 16:55:14 -0800

    timer adjustments

--- game.py
+++ game.py
@@ -5,20 +5,26 @@ gen = DocumentGenerator()
 
 # measure elapsed time
 import time
-start = time.time()
 
 
 # print a paragraph for user to type
 print()
+time.sleep(1)
 print("When you see \"GO!\", Type in the sentence below as fast as you can!")
-
+time.sleep(2)
 sentence_to_type = gen.sentence()
 print(sentence_to_type) 
 
 # sleep 3 seconds before starting 
-time.sleep(3)
+time.sleep(4)
+start = time.time()
 print("GO!")
 
+while True:
+    pass
+
+
+# break loop when match 
 # user done typing sentence so end time
 end = time.time()
 print(end - start)
